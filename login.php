b56bf6af57c878c964d4fe43b501177796037352
Dinesh Kumar <mapmydestination@gmail.com>
2024-12-12 12:47:15 +0530

    update

--- login.php
+++ login.php
@@ -7,9 +7,7 @@ include('./db_connect.php');
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
   <title>Login | Customer Support System</title>
-
 <?php include('./header.php'); ?>
 <?php 
 if(isset($_SESSION['login_id']))
